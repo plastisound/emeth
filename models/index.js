50d98bcddcf81e6dacd2f1de0df1513eb5488cd9
Gerardo <gerardo@mbp-de-mario.lan>
2016-04-19 10:08:18 -0500

    movida 1

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ var fs        = require('fs')
 
 require('sequelize-hierarchy')(Sequelize);
 
-if (process.env.NODE_ENV == 'production'){
+/*if (process.env.NODE_ENV == 'production'){
   console.log('Production');
   sequelize = new Sequelize('emethma', 'emethma', '3[hVRKbN*+', {
     host: 'conricyt.cs8atrvkgeb4.us-west-2.rds.amazonaws.com', 
@@ -15,16 +15,16 @@ if (process.env.NODE_ENV == 'production'){
     logging: false,
     protocol: 'mysql',
     port:    3306 // or 5432 (for postgres),
-  });
-  /*if (process.env.NODE_ENV == 'production'){
+  });*/
+  if (process.env.NODE_ENV == 'production'){
   console.log('Production');
-  sequelize = new Sequelize('kbufojdb_eme', 'kbufojdb_emdbusr', 'WMOx)oACch-d', {
-    host: 'earth.sinergiahosting.com', 
+  sequelize = new Sequelize('actaot9q_emetv1', 'actaot9q_usr1', 'T7w{l&weZGZr', {
+    host: 'actao.org.md-56.webhostbox.net:2082', 
     dialect:  'mysql',
     logging: false,
     protocol: 'mysql',
     port:    3306 // or 5432 (for postgres),
-  });*/
+  });
 } else {
   console.log('Development');
   sequelize = new Sequelize('emethma', 'emethma', '3[hVRKbN*+', {
